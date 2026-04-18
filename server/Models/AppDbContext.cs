d5ec77ef633764aa7e5cc3b47effbe3bed2162f6
toghrulabdullayev <toghrulabdullayev1@gmail.com>
2026-04-19 03:58:07 +0400

    Add initial setup for ASP.NET Core Identity with Entity Framework
    - Updated notes with .NET CLI commands for project setup and EF migrations.
    - Configured appsettings.json for database connection.
    - Added necessary NuGet packages in server.csproj.
    - Created AppDbContext for Identity integration.
    - Implemented initial migration for Identity tables.
    - Added model snapshot for EF Core.
    - Included .gitignore for Visual Studio Code settings.

---
+++ server/Models/AppDbContext.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Server.Models
+{
+  public class AppDbContext : IdentityDbContext
+  {
+    // base(options) calls the parent constructor, equivalent of super(...) in Java
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+
+    }
+  }
+}
--- server/Program.cs
+++ server/Program.cs
@@ -1,5 +1,11 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Server.Models;
+
 // creates DI container, config system, logging, hosting environment and etc
 var builder = WebApplication.CreateBuilder(args);
+
+//* makes them available
 
 // registers controllers, MVC system
 builder.Services.AddControllers();
@@ -10,8 +16,18 @@ builder.Services.AddEndpointsApiExplorer();
 // registers Swagger generator (OpenAPI docs)
 builder.Services.AddSwaggerGen();
 
+// registers identity services (auth, entities, and etc. Refer to Identity architecture docs)
+builder.Services
+  .AddIdentityApiEndpoints<IdentityUser>()
+  .AddEntityFrameworkStores<AppDbContext>();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+        options.UseSqlServer(builder.Configuration.GetConnectionString("DevDB")));
+
 // creates the actual application
 var app = builder.Build();
+
+//* activates their behavior in pipeline
 
 // adds middleware for generating API docs and serving Swagger UI in dev mode
 if (app.Environment.IsDevelopment())
